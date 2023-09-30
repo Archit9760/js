b074d2f0f65cfbfeebb498538e2103dbd8b69376
Archit9760 <139844862+Archit9760@users.noreply.github.com>
2023-09-30 03:30:14 +0000

    new file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "archit@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+
+//accountId = 2 //const means that we cannot change the assign value//
+//
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "Bengluru"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
