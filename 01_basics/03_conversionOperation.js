59e44a99397990062d07c23a5e4e152af94cbe84
Archit9760 <139844862+Archit9760@users.noreply.github.com>
2023-10-03 10:03:01 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,8 +28,27 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+//****************Operations****************************** */
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = " archit"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2); => 12
+// console.log(1 + "2"); => 12
+// console.log("1" + 2 + 2); => 122
+// console.log(1 + 2 + "2"); => 32
+
+console.log((3 + 4) * 5 % 3);
