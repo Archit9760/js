8826396d43b6506a369d33c778e2e7034eff9126
Archit9760 <139844862+Archit9760@users.noreply.github.com>
2023-10-03 09:52:10 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = true
+
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; False => 0;
+
+
+
+let isLoggedIn = "Archit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+//"archit" => true
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
