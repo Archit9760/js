d5d3ee1a60291dc947dec58bc753f2993f562a70
Archit9760 <139844862+Archit9760@users.noreply.github.com>
2023-10-03 06:38:14 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treae all js code as newer version//
+
+//alert("hello");//we are using node.js not browser//
+
+console.log(3 + 3); 
+console.log("Archit");
+//code readability should be high//
+
+let name = "Archit" //String DataTypes
+let age = 18 //number
+
+//primitive datatypes//
+
+//number => 2 to power 53
+//bigint
+//string=> ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+// symbol => finding uniqueness
+
+
+
+//object => 
+console.log(typeof age); //undefined
+console.log(typeof null); //object
