2a341f824ea5e6462363b4d1742e6638a53576a9
Archit9760 <139844862+Archit9760@users.noreply.github.com>
2023-10-16 05:20:42 +0000

    datatypes

---
+++ datatypes-summary.js
@@ -0,0 +1,22 @@
+// # primitive
+
+/* 7 categories : String, number, boolearn, null, undefined, symbol, bigint */
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+//symbol declaration//
+const id = symbol('123')
+const anotherId = symbol('123')
+
+console.log(id === anotherId);
+
+
+// Refrence Type or Non primitive//
+
+//Array, Objects, Functions//
+
