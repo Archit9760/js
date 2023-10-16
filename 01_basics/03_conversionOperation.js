e1b9840709e3835140b149ec19526ebd473e56a7
Archit9760 <139844862+Archit9760@users.noreply.github.com>
2023-10-16 04:39:55 +0000

    comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,9 +16,9 @@
 
 
 
-let isLoggedIn = "Archit"
+// let isLoggedIn = "Archit"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
 
@@ -36,19 +36,31 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 //****************Operations****************************** */
 
 
-let value = 3
-let negValue = -value
-// console.log(negValue);
+// let value = 3
+// let negValue = -value
+// // console.log(negValue);
 
 
-let str1 = "hello"
-let str2 = " archit"
-let str3 = str1 + str2
-console.log(str3);
+// let str1 = "hello"
+// let str2 = " archit"
+// let str3 = str1 + str2
+// console.log(str3);
 
-// console.log("1" + 2); => 12
-// console.log(1 + "2"); => 12
-// console.log("1" + 2 + 2); => 122
-// console.log(1 + 2 + "2"); => 32
+// // console.log("1" + 2); => 12
+// // console.log(1 + "2"); => 12
+// // console.log("1" + 2 + 2); => 122
+// // console.log(1 + 2 + "2"); => 32
 
-console.log((3 + 4) * 5 % 3);
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(true);
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+
+// let gameCounter = 100
+// ++gameCounter;
+
+// console.log(gameCounter);
